c16b6f17f4be5a233851dd04541f0583999869a3
MarioEliasER <201G0247@rcarbonifera.tecnm.mx>
2024-04-09 17:16:52 -0600

    c

---
+++ MensajesClientHTTP/Services/MensajesService.cs
@@ -0,0 +1,21 @@
+﻿using MensajesClientHTTP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajesClientHTTP.Services
+{
+    public class MensajesService
+    {
+        public async void EnviarMensaje(ServerModel server, MensajeDTO mensaje)
+        {
+            var url = $"http://{server.IPEndPoint.Address.ToString()}:7002/mensajitos/?texto={mensaje.Texto}&colorletra={mensaje.ColorLetra}&colorfondo={mensaje.ColorFondo}";
+            HttpClient cliente = new();
+            await cliente.GetStringAsync(url);
+        }
+    }
+}
